23a7a361a85d4c8ccb917c8cd0e584e46ad98c2b
Niklaus-Mk <niklausmikaelsonboss@gmail.com>
2022-08-11 17:24:03 -0300

    inicializacion del projecto

---
+++ src/main/java/com/miproject/miproject/MiprojectApplication.java
@@ -0,0 +1,14 @@
+package com.miproject.miproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiprojectApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/miproject/miproject/MiprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miproject.miproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
